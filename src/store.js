ba40591838d250af61c006f087fdf48e01b89ffa
kirangarrix <kirangarrix2019@gmail.com>
2022-02-06 20:46:55 +0530

    add and remove todo fn added

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { createStore,combineReducers,applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import{ TodoReducer} from './reducer/TodoReducers'
 const reducer=combineReducers({
     // contain all reducer
+    Todo:TodoReducer
    
 })
 
